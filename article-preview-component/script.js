fe18422d5c45a13282255036e1c5d86424ab7efb
Gerald Teo <geraldteo7@gmail.com>
2023-07-29 02:46:39 +0800

    looks okay

--- article-preview-component/script.js
+++ article-preview-component/script.js
@@ -1,10 +1,29 @@
 let button = document.getElementsByClassName("share-btn")[0]
 let mobileTooltip = document.getElementsByClassName("share-mobile-container")[0]
-button.onclick = () => {
-	console.log(screen.width);
-	if (screen.width <= 720) {
+let desktopTooltip = document.getElementsByClassName("desktop-tooltip")[0]
+
+window.onresize = () => {
+	if (window.innerWidth > 720) {
+		mobileTooltip.style.visibility = "hidden"
+	}
+}
+
+button.onfocus = () => {
+	if (window.innerWidth <= 720) {
 		mobileTooltip.style.visibility = (getComputedStyle(mobileTooltip).visibility === "hidden")
 										? "visible"
 										: "hidden"
 	}
+}
+
+button.onblur = () => {
+	if (window.innerWidth <= 720) {
+		mobileTooltip.style.visibility = (getComputedStyle(mobileTooltip).visibility === "hidden")
+										? "visible"
+										: "hidden"
+	}
+}
+
+desktopTooltip.onclick = () => {
+	button.blur()	
 }
