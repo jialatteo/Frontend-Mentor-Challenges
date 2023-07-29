b24c92078d556d53727560f244b5189af784bac3
Gerald Teo <geraldteo7@gmail.com>
2023-07-29 21:15:54 +0800

    bruh:

--- ping-coming-soon-page/script.js
+++ ping-coming-soon-page/script.js
@@ -1,6 +1,6 @@
-document.addEventListener('invalid', customError)
-let emailInput = document.getElementsByClassName("email-input")[0]
+// document.addEventListener('invalid', customError)
+// let emailInput = document.getElementsByClassName("email-input")[0]
 
-function customError(event) {
-	event.preventDefault()
-}
+// function customError(event) {
+// 	event.preventDefault()
+// }
