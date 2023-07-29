0e99e5e12528e880722869d3e88af8ce92778ed6
Gerald Teo <geraldteo7@gmail.com>
2023-07-29 21:34:25 +0800

    done with desktop

--- ping-coming-soon-page/script.js
+++ ping-coming-soon-page/script.js
@@ -1,6 +1,16 @@
-// document.addEventListener('invalid', customError)
-// let emailInput = document.getElementsByClassName("email-input")[0]
+let emailInput = document.getElementsByClassName("email-input")[0]
+let errorMessage = document.getElementsByClassName("error-message")[0]
 
-// function customError(event) {
-// 	event.preventDefault()
-// }
+function customError(e) {
+	e.preventDefault()
+	errorMessage.style.visibility = "visible"
+	emailInput.style.border = "1px red solid"
+	emailInput.style.outline = "none"
+}
+emailInput.addEventListener('invalid', customError)
+
+function inputStyle(e) {
+	errorMessage.style.visibility = "hidden"
+	emailInput.style.border = "	1px solid hsl(223, 87%, 63%)"
+}
+emailInput.addEventListener('input', inputStyle)
