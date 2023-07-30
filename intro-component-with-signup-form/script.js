679c67e31da7fb3b5cfebaea104d0e5bc11cecdd
Gerald Teo <geraldteo7@gmail.com>
2023-07-30 15:35:11 +0800

    done with form validation

--- intro-component-with-signup-form/script.js
+++ intro-component-with-signup-form/script.js
@@ -1,9 +1,31 @@
-let firstName = document.getElementsByClassName("first-name")[0]
-let lastName = document.getElementsByClassName("last-name")[0]
-let email = document.getElementsByClassName("email")[0]
-let password = document.getElementsByClassName("password")[0]
+let inputContainers = document.getElementsByClassName("input-container")
 
-firstName.oninvalid = (e) => {
-	e.preventDefault()	
+for (let i = 0; i < inputContainers.length; i++) {
+	let input = inputContainers[i].getElementsByTagName("input")[0]
+	let placeholder = input.placeholder
+	input.oninvalid = (e) => {
+		input.style.border = "1px red solid"
+		input.style.outline = "none"
+		input.placeholder = ""
 
+		e.preventDefault()
+		let errorMessage = inputContainers[i].getElementsByTagName("p")[0]
+		errorMessage.style.visibility = "visible"
+
+		let errorIcon = inputContainers[i].getElementsByTagName("svg")[0]
+		errorIcon.style.visibility = "visible"
+	}
+
+	input.oninput = () => {
+		if (input.value == "") {
+			input.placeholder = placeholder
+		}
+		input.style.border = "1px solid hsl(248, 32%, 49%)"
+
+		let errorMessage = inputContainers[i].getElementsByTagName("p")[0]
+		errorMessage.style.visibility = "hidden"
+
+		let errorIcon = inputContainers[i].getElementsByTagName("svg")[0]
+		errorIcon.style.visibility = "hidden"
+	}
 }
