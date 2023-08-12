7ebbb2caaa74c8c5c62ed5628523bacc3ea9c80d
Gerald Teo <geraldteo7@gmail.com>
2023-08-12 22:48:49 +0800

    ok done

--- 2-expenses-chart-component/script.js
+++ 2-expenses-chart-component/script.js
@@ -1,10 +1,29 @@
 async function printJSON() {
 	const response = await fetch("data.json")
 	const json = await response.json()
-	console.log(json)
-	console.log(json[0])
-	console.log(json[0].day)
-	console.log(json[0].amount)
+	return json
 }
 
-printJSON()
+let jsonData = printJSON()
+let charts = document.querySelectorAll(".chart")
+const MAX_CHART_HEIGHT = 200
+jsonData.then((data) => {
+	let highestAmount = 0
+	
+	for (let i = 0; i < data.length; i++) {
+		charts[i].amount = data[i].amount
+		const tooltip = charts[i].querySelector(".tooltip")
+		tooltip.textContent = charts[i].amount
+		if (charts[i].amount > highestAmount) {
+			highestAmount = charts[i].amount
+		}
+	}
+
+	charts.forEach(chart => {
+		let chartHeight = (chart.amount / highestAmount) * MAX_CHART_HEIGHT
+		chart.style.height= chartHeight + "px"
+		if (chart.amount == highestAmount) {
+			chart.classList.add("max-chart")
+		}
+	});
+})
